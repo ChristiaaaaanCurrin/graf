bd1a969db80116c1faa400f5a7ab3304f70f8382
Christiaan van de Sande <christiaanvandesande@Christiaans-MacBook-Air.local>
2021-11-30 13:50:57 -0700

    completely reimplemented for vertex deletion

---
+++ graph.c
@@ -0,0 +1,172 @@
+
+// graph definition
+typedef struct vertex {
+  list_t *neighborhood;
+  list_t *in_neighborhood;
+} vertex_t;
+
+typedef struct graph {
+  list_t *vertex_set;
+} graph_t;
+
+// graph queries
+list_t *neighborhood( vertex_t *v ) {
+  return (*v).neighborhood;
+}
+
+list_t *in_neighborhood( vertex_t *v ) {
+  return (*v).in_neighborhood;
+}
+
+size_t order( graph_t *g ) {
+  return length( (*g).vertex_set );
+}
+
+list_t *vertex_set( graph_t *g ) {
+  return (*g).vertex_set;
+}
+
+size_t degree( vertex_t *v ) {
+  return length( neighborhood( v ) );
+}
+
+size_t in_degree( vertex_t *v ) {
+  return length( in_neighborhood( v ) );
+}
+
+int adjacent( vertex_t *v, vertex_t *u ) {
+  return 0 <= list_index( neighborhood( v ), u );
+}
+
+// graph modification
+void link_m( vertex_t *v, vertex_t *u ) {
+  add_first(    neighborhood( v ), u );
+  add_first( in_neighborhood( u ), v );
+}
+
+void link_d( vertex_t *v, vertex_t *u ) {
+  if (!adjacent( v, u ) ) {
+    add_first(    neighborhood( v ), u );
+    add_first( in_neighborhood( u ), v );
+  }
+}
+
+void link_u( vertex_t *v, vertex_t *u ) {
+  if (!adjacent( v, u ) ) {
+    add_first(    neighborhood( v ), u );
+    add_first( in_neighborhood( u ), v );
+  }
+  if (!adjacent( v, u ) ) {
+    add_first(    neighborhood( u ), v );
+    add_first( in_neighborhood( v ), u );
+  }
+}
+
+void loop_m( vertex_t *v ) {
+  link_m( v, v );
+}
+
+void loop_d( vertex_t *v ) {
+  link_d( v, v );
+}
+
+void loop_u( vertex_t *v ) {
+  link_u( v, v );
+}
+
+void unlink_m( vertex_t *v, vertex_t *u ) {
+  remove_one(    neighborhood( v ), u );
+  remove_one( in_neighborhood( u ), v );
+}
+
+void unlink_d( vertex_t *v, vertex_t *u ) {
+  remove_all(    neighborhood( v ), u );
+  remove_all( in_neighborhood( u ), v );
+}
+
+void unlink_u( vertex_t *v, vertex_t *u ) {
+  remove_all(    neighborhood( v ), u );
+  remove_all( in_neighborhood( u ), v );
+  remove_all(    neighborhood( u ), v );
+  remove_all( in_neighborhood( v ), u );
+}
+
+void clear_vertex( vertex_t *v ) {
+  node_t *p = (*neighborhood( v )).head;
+  while ( p != NULL ) {
+    unlink_u( v, (*p).value );
+    p = (*p).next;
+  }
+}
+
+void delete_vertex( graph_t *g, vertex_t *v ) {
+  clear_vertex( v );
+  remove_all( (*g).vertex_set, v );
+  free( neighborhood( v ) );
+  free( in_neighborhood( v ) );
+  free( v );
+}
+
+// graph commands
+void print_graph( graph_t *g ) {
+  node_t *p = first_node( (*g).vertex_set );
+  while ( p != NULL ) {
+    printf( "%zu\t", (size_t) (*p).value );
+    print_list( neighborhood( (*p).value ) );
+    p = (*p).next;
+  }
+}
+
+vertex_t *empty_vertex(){
+  vertex_t *v = malloc( sizeof( vertex_t ) );
+  *v = (vertex_t) { new_list(), new_list() };
+  return v;
+}
+
+vertex_t *copy_vertex( vertex_t *v ) {
+  vertex_t *u = malloc( sizeof( vertex_t ) );
+  *u = (vertex_t) { copy_list( neighborhood( v ) ), copy_list( in_neighborhood( v ) ) };
+  return u;
+}
+
+graph_t *empty_graph( size_t order ) {
+  graph_t *g = malloc( sizeof( graph_t ) );
+  list_t *vs = new_list();
+  for (int i = 0; i < order; i++ ) {
+    add_first( vs, empty_vertex() );
+  }
+  *g = (graph_t) { vs };
+  return g;
+}
+
+graph_t *path_graph( size_t order ) {
+  graph_t *g = empty_graph( order );
+  node_t *p = first( (*g).vertex_set );
+  node_t *q;
+  while ( p != NULL ) {
+    q = (*p).next;
+    if ( q != NULL ) {
+      link_u( (*p).value, (*q).value );
+    }
+    p = (*p).next;
+  }
+  return g;
+}
+
+graph_t *complete_graph( size_t order ) {
+  graph_t *g = empty_graph( order );
+  node_t *p = first( (*g).vertex_set );
+  node_t *q;
+  while ( p != NULL ) {
+    q = (*p).next;
+    while ( q != NULL ) {
+      if ( (*p).value != (*q).value ) {
+        link_u( (*p).value, (*q).value );
+      }
+      q = (*q).next;
+    }
+    p = (*p).next;
+  }
+  return g;
+}
+
---
+++ list.c
@@ -0,0 +1,179 @@
+
+typedef void *v_t;
+
+// list definition
+
+typedef struct node {
+  v_t value;
+  struct node *next;
+} node_t;
+
+typedef struct list {
+  struct node *head;
+  struct node *tail;
+} list_t;
+
+// error behavior
+void empty_list_error() {
+ printf( "EMPTY LIST ERROR\n"  );
+}
+
+// list commands
+void print_list( list_t *list ) {
+  printf("{");
+  if ( (*list).head == NULL ) {
+    printf( "}\n" );
+  } else {
+    node_t *p = (*list).head;
+    while (p != NULL) {
+      printf( " %zu,", (size_t) (*p).value );
+      p = (*p).next;
+    }
+    printf("\b }\n");
+  }
+}
+
+list_t *new_list() {
+  list_t *list = malloc( sizeof( list_t ) );
+  *list = (list_t) { NULL, NULL };
+  return list;
+}
+
+// list queries
+size_t length( list_t *list ) {
+  size_t len = 0;
+  node_t *p = (*list).head;
+  while (p != NULL) {
+    p = (*p).next;
+    len++;
+  }
+  return len;
+}
+
+node_t *first_node( list_t *list ) {
+  return (*list).head;
+}
+
+node_t *last_node( list_t *list ) {
+  return (*list).tail;
+}
+
+v_t first( list_t *list ) {
+  if ( (*list).head != NULL ) {
+    return (*(*list).head).value;
+  } else {
+    empty_list_error();
+    return NULL;
+  }  
+}
+
+v_t last( list_t *list ) {
+  if ( (*list).tail != NULL ) {
+    return (*(*list).tail).value;
+  } else {
+    empty_list_error();
+    return NULL;
+  }  
+}
+
+int list_index( list_t *list, v_t v ) {
+  node_t *p = (*list).head;
+  for (int i = 0; p != NULL; i++) {
+    if ( (*p).value == v ) {
+      return i;
+    }
+    p = (*p).next;
+  }
+  return -1;
+}
+
+// list modification
+void add_first( list_t *list, v_t value ) {
+  node_t *node = malloc( sizeof( node_t ) );
+  *node = (node_t) { value, (*list).head };
+
+  if ( (*list).head == NULL ) {
+    (*list).tail = node;
+  }
+  (*list).head = node;
+}
+
+void add_last( list_t *list, v_t value ) {
+  node_t *node = malloc( sizeof( node_t ) );
+  *node = (node_t) { value, NULL };
+  if ( (*list).head == NULL ) {
+    (*list).head = node;
+  } else {
+    (*(*list).tail).next = node;
+    (*list).tail = node;
+  }
+}
+
+v_t take_first( list_t *list ) {
+  if ( (*list).head != NULL ) {
+    node_t *p = (*list).head;
+    (*list).head = (*p).next;
+    v_t v = (*p).value;
+    free( p );
+    return v;
+  } else {
+    empty_list_error();
+    return NULL;
+  }
+}
+
+v_t take_last( list_t *list ) {
+  if ( (*list).tail != NULL ) {
+    node_t *p = (*list).tail;
+    v_t v = (*p).value;
+    free( p );
+    node_t *q = (*list).head;
+    while ( (*q).next != NULL ) {
+    q = (*q).next;
+    }
+    (*list).tail = q;
+    return v;
+  } else {
+    empty_list_error();
+    return NULL;
+  }
+}
+
+void remove_all( list_t *list, v_t v ) { // warning: this method violates the order of the list!
+  node_t *p = (*list).head;
+  while ( p != NULL ) {
+    if ( (*p).value == v ) {
+      (*p).value = first( list );
+      take_first( list );
+    }
+    p = (*p).next;
+  }
+}
+
+void remove_one( list_t *list, v_t v ) { // warning:this method violates the order of the list!
+  node_t *p = (*list).head;
+  while ( p != NULL ) {
+    if ( (*p).value == v ) {
+      (*p).value = first( list );
+      take_first( list );
+      return;
+    }
+    p = (*p).next;
+  }
+}
+
+void clear_list( list_t *list ) {
+  while ( (*list).head != NULL ) {
+    take_first( list );
+  }
+}
+
+list_t *copy_list( list_t* list ) {
+  list_t *list2 = new_list();
+  node_t *p = (*list).head;
+  while ( p != NULL ) {
+    add_last( list2, (*p).value );
+    p = (*p).next;
+  }
+  return list2;
+}
